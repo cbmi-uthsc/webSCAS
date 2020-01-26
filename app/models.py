cd113d2d58ff5e73ffb927de85a2e7f466739ea5
mukeshsihag <sihagmukesh22@gmail.com>
2020-01-26 23:36:26 +0530

    added table to detect sepsis

--- app/models.py
+++ app/models.py
@@ -1,13 +1,26 @@
-from app import db
+from app import db, app
+import flask_whooshalchemy as wa
 
 class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=1)
-    patient_name = db.Column(db.String(64), index=1)
-    patient_id = db.Column(db.String(64), index=1)
-    patient_disease = db.Column(db.String(128), index=1)
+    # id = db.Column(db.Integer, primary_key=1)
+    __searchable__ = ['patien_name', 'patient_disease']
+    patient_id = db.Column(db.Integer, primary_key=1)
+    patient_name = db.Column(db.String(64))
+    patient_disease = db.Column(db.String(128))
+    sepsis = db.relationship('Sepsis', backref='patient', lazy='dynamic')
 
     def __repr__(self):
         return '<Patient {}>'.format(self.patient_id)
+    
+wa.whoosh_index(app, Patient)
+class Sepsis(db.Model):
+    s_id = db.Column(db.Integer, primary_key=1)
+    fever = db.Column(db.Integer)
+    heart_rate = db.Column(db.Integer)
+    respiratory_rate = db.Column(db.Integer)
+    abnormal_white_blood_cell_count = db.Column(db.Integer)
+    paco2 = db.Column(db.Integer)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.patient_id'))
 
 # def load_user(id):
 #     return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request
-from app.forms import PatientForm
-from app.models import Patient
+from app.forms import PatientForm, SepsisForm
+from app.models import Patient, Sepsis
 from app import app, db
+import flask_whooshalchemy as wa
 
 @app.route('/')
 @app.route('/index')
@@ -26,6 +27,41 @@ def add_patient():
             patient = Patient(patient_name=form.patient_name.data, patient_id=form.patient_id.data, patient_disease=form.patient_disease.data)
             db.session.add(patient)
             db.session.commit()
-            flash('Your data has been saved successfulluy', 'success')
+            flash('Your data has been saved successfully', 'success')
             return redirect(url_for('index'))
     return render_template('add_patient.html',title='Add Patient', form=form)
+
+@app.route('/patient_details', methods=['GET', 'POST'])
+def patient_details():
+    form = SepsisForm()
+    if request.method == 'GET':
+        return render_template('patient_details.html', title='Patient Details', form=form)
+    elif form.validate_on_submit():
+        patient_id = Patient.query.filter_by(patient_id=form.patient_id.data).first()
+        if patient_id: 
+            s_id = Sepsis.query.filter_by(patient_id=form.patient_id.data).first()
+            if not s_id:
+                details = Sepsis(fever=form.fever.data, heart_rate=form.heart_rate.data, respiratory_rate=form.respiratory_rate.data, abnormal_white_blood_cell_count=form.abnormal_white_blood_cell_count.data, paco2=form.paco2.data, patient_id=form.patient_id.data)
+                db.session.add(details)
+                db.session.commit()
+                flash('Details has been saved', 'success')
+            else:
+                s_id.fever=form.fever.data
+                s_id.heart_rate=form.heart_rate.data
+                s_id.respiratory_rate=form.respiratory_rate.data
+                s_id.abnormal_white_blood_cell_count=form.abnormal_white_blood_cell_count.data
+                s_id.paco2=form.paco2.data
+                db.session.commit()
+                flash("Details has been updated", 'success')
+        else:
+            flash('No Patient exist with Patient ID {}'.format(form.patient_id.data), 'warning')
+    return render_template('patient_details.html', title='Patient Details', form=form)
+
+# @app.route('/search')
+# def search():
+#     data = request.args.get('query')
+#     # print(data)
+#     patients = Patient.query.whoosh_search(data).all()
+#     print(type(patients))
+#     return render_template('index.html', patients=patients)
+#     # return 'hi'
